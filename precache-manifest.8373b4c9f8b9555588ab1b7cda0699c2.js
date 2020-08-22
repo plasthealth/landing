c2e3f8a7277ca9a3b5482469c39e70d2a958c580
Raymond Chen <rc284@duke.edu>
2020-08-22 15:07:40 -0400

    Updates

---
+++ precache-manifest.8373b4c9f8b9555588ab1b7cda0699c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f917de16b62485f6a9b9628cccd2e76c",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "ea4f27edba321a1f2c14",
+    "url": "/landing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c93481fcf215db708c65",
+    "url": "/landing/static/js/2.73b275b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landing/static/js/2.73b275b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4f27edba321a1f2c14",
+    "url": "/landing/static/js/main.6491a392.chunk.js"
+  },
+  {
+    "revision": "c7b2fc2db9c09d6585ab",
+    "url": "/landing/static/js/runtime-main.f0c0ae33.js"
+  },
+  {
+    "revision": "5ee3c9c3096f298c61cae2b742a0281b",
+    "url": "/landing/static/media/logo-title.5ee3c9c3.svg"
+  },
+  {
+    "revision": "6523a2d09a985695278e922ede4286ae",
+    "url": "/landing/static/media/logo.6523a2d0.svg"
+  }
+]);
